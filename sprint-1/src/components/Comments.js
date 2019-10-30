50dd4d6d49772361a947c67915daeb603d4791a8
Alyssa Kwong-Taylor <a.kwong-taylor@outlook.com>
2019-10-30 16:12:06 -0700

    Finished Comments section, responsive @ all lvls.

--- sprint-1/src/components/Comments.js
+++ sprint-1/src/components/Comments.js
@@ -1,30 +1,56 @@
 import React from 'react'; 
-import MohanMuruge from "../assets/Images/Mohan-muruge.jpg"; 
+import mohanMuruge from "../assets/Images/Mohan-muruge.jpg"; 
+import deleteIcon from "../assets/Icons/SVG/icons8-trash.svg"; 
+import likeIcon from "../assets/Icons/PNG/icons8-facebook-like-24.png"; 
 
-export default function Comments() {
+export default function Comments(props) {
+    const thread = props.data.comments.map((comment, index) => {
+        return (
+            <div key={index} className="conversation__comment">
+                <div className="conversation__avatar-wrapper">
+                    <img className="conversation__avatar" alt="" />
+                </div>
+                    <div className="conversation__comment-text">
+                        <div className="conversation__comment-id">
+                            <div className="conversation__comment-name">{comment.name}</div>
+                            <div className="conversation__comment-date">{comment.timestamp}</div>
+                        </div>
+                    <div className="conversation__comment-msg">{comment.msg}</div>
+                    <div className="conversation__comment-tools">
+                        <button className="conversation__tool-icon delete" id="9a353dc5-6316-41ab-a1ea-c90f75b8be85">
+                            <img src={deleteIcon} alt="Delete icon" />
+                        </button>
+                        <div className="conversation__likes">
+                            <button className="conversation__tool-icon like" id="9a353dc5-6316-41ab-a1ea-c90f75b8be85">
+                                <img src={likeIcon} alt="Like icon" />
+                            </button>
+                            <span></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    })
+
     return (
         <section className="conversation">
-            <h1 className="conversation__header">Join the Conversation</h1>
+            <h2 className="conversation__header">3 Comments</h2>
             <div className="conversation__wrapper">
                 <div className="conversation__form">
                     <form className="conversation__input" id="conversation__input">
                         <div className="conversation__avatar-wrapper">
-                            <img name ="avatar" className="conversation__avatar" src={MohanMuruge} alt="Upload an Avatar here for your comments!" />
+                            <img name ="avatar" className="conversation__avatar" src={mohanMuruge} alt="Upload an Avatar here for your comments!" />
                         </div>
                         <div className="conversation__field-wrapper">
+                            <label className ="conversation__field-header" htmlFor="msg">Join the conversation</label>   
                             <div className="conversation__field">
-                                <label className ="conversation__field-header" htmlFor="name">Name</label>
-                                <input className="conversation__fill" type="text" id="name" name="name" placeholder="Mohan Muruge"></input> 
+                                <textarea className="conversation__fill" id="msg" name="comment" placeholder="That was most easily the most spectacular BMX moment ever"></textarea>
+                                <button type="submit" className="conversation__cta">Comment</button>
                             </div>
-                            <div className="conversation__field">
-                                <label className ="conversation__field-header" htmlFor="msg">Comment</label>
-                                <textarea className="conversation__fill" id="msg" name="comment" rows="6" placeholder="Add a new comment"></textarea>
-                            </div>
-                            <button type="submit" className="conversation__cta">Comment</button>
                         </div>
                     </form>
                 </div>
-                <div className="conversation__thread"></div>
+                <div className="conversation__thread">{thread}</div>
             </div>
         </section>
     )
--- sprint-1/src/components/MainContent.js
+++ sprint-1/src/components/MainContent.js
@@ -1,16 +1,54 @@
 import React from 'react'; 
+// import axios from 'axios'; 
 import MainVideo from "./MainVideo"; 
 import VideoInfo from "./VideoInfo"; 
 import Comments from "./Comments"; 
 
-export default function MainContent(props) {
-    const data = props.data;  
+export default function MainContent() {  
+    /// Axios not used in this sprint. Commented out, but kept in for later reference for now.
+    // const projectKey = "?api_key=2bb7dd6d-ca82-47b6-9411-0b448a4f3395"; 
+    // const commentData = 
+    //     Axios.get("https://project-1-api.herokuapp.com/comments" + projectKey) 
+    //         .then((resp) => {
+    //             return resp.data; 
+    //         })
+
+    const commentData = [
+        {name: "Micheal Lyons", 
+        timestamp: "12/18/2018", 
+        msg: "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed."
+        }, 
+  
+        {name: "Gary Wong", 
+        timestamp: "12/18/2018", 
+        msg: "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He's so talented! I wish I can ride like him one day so I can really enjoy myself."
+        }, 
+  
+        {name: "Theodore Duncan", 
+        timestamp: "12/18/2018", 
+        msg: "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He's definitely my favourite ever!"
+        }
+    ]
+  
+    const mainVideo = {
+        id: "redCowBMX", 
+        image: "../assets/Images/video-list-0.jpg",
+        clip: "../assets/Video/BrainStation-Sample-Video.mp4",
+        duration: "00:00:20", 
+        title: "BMX Rampage 2018 Highlights", 
+        channel: "Red Cow", 
+        timestamp: "12/18/2018", // Actual ms epoch --> 1545120000000
+        views: "1,001,023", 
+        likes: "110,985",
+        description: "On a gusty day in Southern Utah, a group of 25 daring mountain bikers blew the doors off what is possible on two wheels, unleashing some of the biggest moments the sport has ever seen. While mother nature only allowed for one full run before the conditions made it impossible to ride, that was all that was needed for event veteran Kyle Strait, who won the event for the second time -- eight years after his first Red Cow Rampage title", 
+        comments: commentData, 
+    };
 
     return (
         <main>
             <MainVideo />
-            <VideoInfo data={data}/>
-            <Comments data={data}/>
+            <VideoInfo data={mainVideo}/>
+            <Comments data={mainVideo}/>
         </main>
   );
 }
