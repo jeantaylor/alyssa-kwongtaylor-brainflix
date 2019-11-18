d64fe0f7192e2fc55b9f2a21365af0a22cc90cd7
Alyssa KT <a.kwong-taylor@outlook.com>
2019-11-17 20:05:35 -0800

    Fixed issue of clicking on newly uploaded video not switching out the mainVideo information.

--- sprint-3/server/routes/api/upload.js
+++ sprint-3/server/routes/api/upload.js
@@ -11,27 +11,50 @@ let mainVids = require(mainVideoData);
 router.use(cors());
 
 router.post("/", (req, res) => {
-    const postDate = Date.now().toString(); 
+    const postDate = Date.now(); 
+    const newId = helper.getNewId();
 
     const newVideo = {
-        id: helper.getNewId(),
+        id: newId,
         title: req.body.title,
         channel: "Mohan Murr",
-        image: req.body.image
+        image: "../assets/Images/Upload-video-preview.jpg"
     }
 
     const newVideoDetails = {
-        id: helper.getNewId(),
+        id: newId,
         title: req.body.title,
         channel: "Mohan Murr",
-        image: req.body.image, 
+        image: "../assets/Images/Upload-video-preview.jpg", 
         description: req.body.description, 
         views: "0", 
         likes: "0", 
         duration: "0", 
         video: "", 
-        timestamp: {postDate}, 
-        comments: []
+        timestamp: postDate, 
+        comments: [
+            {
+                "name": "Micheal Lyons",
+                "comment": "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of acconcert I have EVER witnessed.",
+                "id": "1ab6d9f6-da38-456e-9b09-ab0acd9ce818",
+                "likes": 0,
+                "timestamp": 1545162149000
+            },
+            {
+                "name": "Gary Wong",
+                "comment": "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!",
+                "id": "cc6f173d-9e9d-4501-918d-bc11f15a8e14",
+                "likes": 0,
+                "timestamp": 1544595784046
+            },
+            {
+                "name": "Theodore Duncan",
+                "comment": "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!",
+                "id": "993f950f-df99-48e7-bd1e-d95003cc98f1",
+                "likes": 0,
+                "timestamp": 1542262984046
+            }
+        ]
     }
 
     if (!newVideoDetails.title || !newVideoDetails.channel || !newVideoDetails.image || !newVideoDetails.description) {
